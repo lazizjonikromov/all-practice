8ca04d7d743339868ee7ac64ea789f3c6b1b17df
Laziz <lazizjonweb@gmail.com>
2024-06-29 10:15:47 +0500

    todo-list

---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Data 1",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Data 2",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Data 3",
+    completed: false,
+  }
+];
+
